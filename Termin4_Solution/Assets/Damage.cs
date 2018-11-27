6ada181901d51d904da599729ac376e6bccb0af8
HanHan <hauffej@yahoo.de>
2018-11-27 21:23:14 +0100

    Termin4_Solution finished

---
+++ Termin4_Solution/Assets/Damage.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _damageOnEnter;
+    [SerializeField]
+    private float _damageOnStay;
+    [SerializeField]
+    private float _damageOnExit;
+
+    private float _damageOverTime;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _damageOverTime = 0;
+        if (collision.gameObject.tag == "Player")
+        {
+            Health player = collision.gameObject.GetComponent<Health>();
+            if (player != null)
+            {
+                player.AddDamage(_damageOnEnter);
+                _damageOverTime += _damageOnEnter;
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Health player = collision.gameObject.GetComponent<Health>();
+        if (player != null)
+        {
+            player.AddDamage(_damageOnStay);
+            _damageOverTime += _damageOnStay;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Health player = collision.gameObject.GetComponent<Health>();
+        if (player != null)
+        {
+            player.AddDamage(_damageOnExit);
+            _damageOverTime += _damageOnExit;
+        }
+
+        if (_damageOverTime > 0)
+            Debug.Log("Damage: " + _damageOverTime);
+        else if (_damageOverTime < 0)
+            Debug.Log("Healing: " + _damageOverTime * -1);
+    }
+
+}
---
+++ Termin4_Solution/Assets/Health.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    [SerializeField]
+    private float _health;
+    [SerializeField]
+    private float _maxHealth;
+
+
+    public void AddDamage(float damage)
+    {
+        _health -= damage;
+
+        if (_health <= 0)
+        {
+            Debug.Log("I'm dead...");
+            _health = 0;
+        }
+
+        if (_health > _maxHealth)
+            _health = _maxHealth;
+    }
+
+}
