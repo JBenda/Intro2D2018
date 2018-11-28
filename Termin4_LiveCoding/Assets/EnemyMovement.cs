3f20a21daec2084d5a421588d63dfba6eae1d37a
johannes.hauffe <clonkcool@yahoo.de>
2018-11-28 18:40:36 +0100

    Termin4 Livecoding Termin5 Vorlage

---
+++ Termin4_LiveCoding/Assets/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform _following;
+    private Rigidbody2D _rgbd;
+    private float _speed = 60;
+
+
+    private void Start()
+    {
+        _rgbd = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (_rgbd != null && _following != null)
+        {
+            Vector2 dir = _following.position - transform.position;
+            dir.Normalize();
+
+            _rgbd.velocity = dir * _speed * Time.deltaTime;
+        }
+    }
+
+}
---
+++ Termin4_LiveCoding/Assets/ObjChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjChanger : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject targetObj;
+
+    public GameObject targetObj2;
+    public PlayerMovement player;
+    public int a;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" )
+        {
+            Debug.Log("collision with player");
+            if (targetObj.activeInHierarchy)
+            {
+                Debug.Log("Deactivate");
+                targetObj.SetActive(false);
+            }
+            else
+            {
+                Debug.Log("Activate");
+                targetObj.SetActive(true);
+            }
+        }
+    }
+}
