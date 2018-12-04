b8610f5c0d61fba596eadc7388fcd8aae81829f6
HanHan <hauffej@yahoo.de>
2018-12-04 23:48:39 +0100

    Termin_6_Vorlage added

---
+++ Termin6_Vorlage/Assets/Scripts/CreateObj.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateObj : MonoBehaviour {
+        
+    public void Create()
+    {
+        //--TODO--//
+        Debug.Log("Create pressed");
+    }
+
+}
