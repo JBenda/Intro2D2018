1864f7ac6dd8a066d483ed76d11e796627e02399
johannes.hauffe <clonkcool@yahoo.de>
2018-12-19 16:19:08 +0100

    Changed at Termin7_Solution

--- Termin7_Solution/Assets/Scripts/InputControllers/GamePadController.cs
+++ Termin7_Solution/Assets/Scripts/InputControllers/GamePadController.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GamePadController : MonoBehaviour {
+public class GamePadController : AInputController
+{
+    private bool _shotPressed = false;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public override Vector2 GetMoveDir()
+    {
+        Vector2 stick = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if (stick != Vector2.zero)
+            stick.Normalize();
+        return stick;
+    }
+
+    public override Vector2 GetShootDir()
+    {
+        Vector2 stick = new Vector2(Input.GetAxis("HorizontalRight"), Input.GetAxis("VerticalRight"));
+        if (stick != Vector2.zero)
+            stick.Normalize();
+        return stick;
+    }
+
+    public override bool IsShootDown()
+    {
+        if (Input.GetAxis("Fire1") < 0)
+            if (!_shotPressed)
+                return _shotPressed = true;
+            else
+                return false;
+        else
+            return (_shotPressed = false);
+    }
+
+    public override bool IsUseItemDown()
+    {
+        return Input.GetButtonDown("Fire2");
+    }
 }
